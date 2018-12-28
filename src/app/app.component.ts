e1342d9f34d51eb57b1ff0d6ec540373995c66c2
pradeepanvi <pradeepkumar.anvi@gmail.com>
2018-12-28 15:45:38 +0530

    'radius-issue-rest-is-done'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,20 @@ export class AppComponent implements OnInit {
   title: string = 'My first AGM project';
   markerData:any;
   mapData:any;
+  radiusData:any;
 
   lat: number = 51.5135225;
   lng: number = -0.12687700000003588;
+
+  radius: number;
+
+  lat2: number = 51.5073509;
+  lng2: number = -0.12775829999998223;
+
   updateM: number;
+  updateL: number;
   iconUrl: string = 'assets/blue_marker.png'
-  iconUrl2: string = 'assets/orange_marker.png'
+  iconUrl2: string = 'assets/orange_marker.png';
 
   constructor(private http:HttpClient){}
 
@@ -24,18 +32,49 @@ export class AppComponent implements OnInit {
       (res) => {
         this.mapData = res;
         this.markerData = res;
+        this.radiusData = res;
         console.log(res);
+        for(var i=0; i<this.markerData.length; i++){
+          this.markerData[i].iconUrl = this.iconUrl;
+        }
       }
     )
   }
 
-  clickedMarker(index: number) {
+  listclicked(index){
+    for(var i=0; i<this.markerData.length; i++){
+      this.markerData[i].iconUrl = this.iconUrl;
+    }
+    if(this.updateL != index){
+      this.markerData[index].iconUrl = this.iconUrl2;
+    }
+  }
+
+  clickedMarker($event, index: number) {
+    for(var i=0; i<this.markerData.length; i++){
+      this.markerData[i].iconUrl = this.iconUrl;
+    }
     if(this.updateM != index){
       this.mapData = [];
       this.mapData.push(this.markerData[index]);
       this.updateM = index;
+      this.markerData[index].iconUrl = this.iconUrl2;
     } else {
       this.mapData = this.markerData;
+    }
+  }
+
+  radiusChnage($event){
+    this.radius = $event;
+  }
+  dragEnd($event){
+    this.mapData = [];
+    this.markerData = [];
+    for(var i=0; i<this.radiusData.length; i++){
+      if($event.coords.lat + (this.radius / (this.radius * this.radius)) + (this.radius / (this.radius * this.radius)) > this.radiusData[i].lat && $event.coords.lng + (this.radius / (this.radius * this.radius)) + (this.radius / (this.radius * this.radius)) > this.radiusData[i].lon){
+        this.mapData.push(this.radiusData[i]);
+        this.markerData.push(this.radiusData[i]);
+      }
     }
   }
 }
